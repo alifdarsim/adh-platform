8c379197a2788cd75a32d9e850fb5590c9c5b9c8
Admin Alif <alifdarsim@email.com>
2024-03-15 12:28:19 +0800

    add 503 page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.others.main')
+@section('content')
+    <style>
+        #item-center {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            /* Make sure the container takes the full height of the viewport */
+            margin: 0;
+            /* Remove default margin */
+        }
+    </style>
+    <div class="nk-block nk-block-middle wide-md mx-auto" id="item-center">
+        <div class="nk-block-content nk-error-ld text-center">
+            <div>
+                <img class="email-logo mb-5" src="/images/logo-dark.png" alt="logo">
+            </div>
+            <img class="nk-error-gfx w-50 pb-4" src="/images/gfx/error-404.svg" alt="">
+            <div class="wide-xs mx-auto">
+                <h3 class="nk-error-title">Maintainace In Progress</h3>
+                <p class="nk-error-text">We are very sorry for inconvenience. We currently upgrading our system</p>
+            </div>
+        </div>
+    </div>
+@endsection
