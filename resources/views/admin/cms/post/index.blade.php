8d440a7fe7b6e6fc7316c2ff7058151a12d52b1c
alifdarsim <alifdarsim@gmail.com>
2024-03-21 14:58:08 +0800

    cms change

--- resources/views/admin/cms/post/index.blade.php
+++ resources/views/admin/cms/post/index.blade.php
@@ -150,98 +150,22 @@
         </div>
     </div>
 
-{{--    <div class="nk-block">--}}
-{{--        <div class="card card-bordered card-preview">--}}
-{{--            <div class="card-inner">--}}
-{{--                <table id="datatable" class="datatable-init-export nowrap table" data-export-title="Export">--}}
-{{--                    <thead>--}}
-{{--                    <tr class="nk-tb-item nk-tb-head">--}}
-{{--                        <th class="nk-tb-col tb-col-lg"><span class="sub-text">Author</span></th>--}}
-{{--                        <th class="nk-tb-col"><span class="sub-text">Title</span></th>--}}
-{{--                        <th class="nk-tb-col tb-col-mb"><span class="sub-text">Category</span></th>--}}
-{{--                        <th class="nk-tb-col tb-col-md"><span class="sub-text">Status</span></th>--}}
-{{--                        <th class="nk-tb-col tb-col-lg"><span class="sub-text">Insert At</span></th>--}}
-{{--                        <th class="nk-tb-col nk-tb-col-tools text-end"></th>--}}
-{{--                    </tr>--}}
-{{--                    </thead>--}}
-{{--                    <tbody>--}}
-{{--                    @foreach($pages as $page)--}}
-{{--                        <tr>--}}
-{{--                            <td class="nk-tb-col">--}}
-{{--                                <div class="user-card">--}}
-{{--                                    <div class="tw-h-7 tw-w-7 user-avatar bg-dim-primary d-none d-sm-flex !tw-mr-2">--}}
-{{--                                        <span>AB</span>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="user-info">--}}
-{{--                                        <span>{{$page->author->name}}</span>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </td>--}}
-{{--                            <td class="tw-align-middle">{{$page->title}}</td>--}}
-{{--                            <td class="tw-capitalize tw-align-middle">{{$page->type}}</td>--}}
-{{--                            <td class="tw-capitalize tw-align-middle">--}}
-{{--                                <span--}}
-{{--                                    class="tw-my-auto badge bg-{{$page->status == 'published' ? 'success' : ($page->status == 'draft' ? 'warning' : 'secondary')}}">{{$page->status}}--}}
-{{--                                </span>--}}
-{{--                                @if ($page->featured == 1)--}}
-{{--                                    <span class="tw-my-auto badge bg-primary">Featured</span>--}}
-{{--                                @endif--}}
-{{--                            </td>--}}
-{{--                            <td class="tw-align-middle">{{$page->created_at}}</td>--}}
-{{--                            <td class="nk-tb-col nk-tb-col-tools">--}}
-{{--                                <ul class="nk-tb-actions gx-1">--}}
-{{--                                    <li>--}}
-{{--                                        <div class="drodown">--}}
-{{--                                            <a href="#" class="dropdown-toggle btn btn-icon btn-trigger"--}}
-{{--                                               data-bs-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>--}}
-{{--                                            <div class="dropdown-menu dropdown-menu-end">--}}
-{{--                                                <ul class="link-list-opt no-bdr">--}}
-{{--                                                    <li>--}}
-{{--                                                        <a class="tw-cursor-pointer"--}}
-{{--                                                           onclick="quick_view('{{$page->id}}')">--}}
-{{--                                                            <em class="icon ni ni-eye"></em><span>Quick View</span></a>--}}
-{{--                                                    </li>--}}
-{{--                                                    <li><a href="{{$page->url}}" target="_blank"--}}
-{{--                                                           rel="noopener noreferrer"><em--}}
-{{--                                                                class="icon ni ni-article"></em><span>To Post</span></a>--}}
-{{--                                                    </li>--}}
-{{--                                                    <li><a href="{{route('admin.post.index', ["id" => $page->id])}}"><em--}}
-{{--                                                                class="icon ni ni-edit"></em><span>Edit Post</span></a>--}}
-{{--                                                    </li>--}}
-{{--                                                    <li><a class="tw-cursor-pointer"--}}
-{{--                                                           onclick="remove('{{$page->id}}')"><em--}}
-{{--                                                                class="icon ni ni-trash"></em><span>Delete Post</span></a>--}}
-{{--                                                    </li>--}}
-{{--                                                </ul>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                    </li>--}}
-{{--                                </ul>--}}
-{{--                            </td>--}}
-{{--                        </tr>--}}
-{{--                    @endforeach--}}
-{{--                    </tbody>--}}
-{{--                </table>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
-{{--    <div class="modal fade" tabindex="-1" id="modalLarge">--}}
-{{--        <div class="modal-dialog modal-xl" role="document">--}}
-{{--            <div class="modal-content">--}}
-{{--                <div class="modal-header">--}}
-{{--                    <h5 class="modal-title">Post Quick View</h5>--}}
-{{--                    <a class="close" data-bs-dismiss="modal" aria-label="Close"><em class="icon ni ni-cross"></em></a>--}}
-{{--                </div>--}}
-{{--                <div class="modal-body"></div>--}}
-{{--                <div class="modal-footer bg-light">--}}
-{{--                    <p class="sub-text">This is for quick preview only. To view actual content please go to post.--}}
+    <div class="modal fade" tabindex="-1" id="modalLarge">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Post Quick View</h5>
+                    <a class="close" data-bs-dismiss="modal" aria-label="Close"><em class="icon ni ni-cross"></em></a>
+                </div>
+                <div class="modal-body"></div>
+                <div class="modal-footer bg-light">
+                    <p class="sub-text">This is for quick preview only. To view actual content please go to post.
 {{--                        <a href="{{$page->url}}" class="tw-underline tw-text-blue-500 ms-1">View Post</a>--}}
-{{--                    </p>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
 
@@ -252,7 +176,7 @@
 
         datatableInit('#datatable', {
             ajax: '{{route('admin.post.datatable')}}',
-            order:  [[2, 'desc']],
+            order:  [[4, 'desc']],
             columnDefs: [
                 { "className": "nk-tb-col", "targets": "_all" },
                 {
@@ -300,7 +224,6 @@
                                         <a href="#" class="dropdown-toggle btn btn-icon btn-trigger" data-bs-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
                                         <div class="dropdown-menu dropdown-menu-end">
                                             <ul class="link-list-opt no-bdr">
-                                                <li><a onclick="view(${data})"><em class="icon ni ni-eye"></em><span>View</span></a></li>
                                                 <li><a onclick="remove(${data})"><em class="icon ni ni-trash"></em><span>Remove</span></a></li>
                                             </ul>
                                         </div>
